d79b4a266dd12eb930e71eabaa45f19080365835
VadimPST <44578292+VadimPST@users.noreply.github.com>
2019-11-13 13:56:39 +0100

    original

---
+++ src/Adivino.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+    public class Adivino {
+
+        public static void main(String[] args) {
+            int n = 0,cont = 0;
+            int x = (int) (500 *Math.random());
+            //int x = (int)Math.floor((Math.random()*(10-50+1)+50));//valor entre 10 y 50 ambos inclusive
+            //System.out.print(x);
+            Scanner sc = new Scanner(System.in);
+            do {
+                System.out.print("\nIntroduce el valor de X: ");
+                n = sc.nextInt();
+                if (n > x) {
+                    System.out.print("Te pasaste Amigo");
+                } else if (n < x){
+                    System.out.print("Casi cerca ...");
+                }
+                cont++;
+            } while (n != x);
+            System.out.print("Exelente!!! Haz acertado , No. de intentos: "+cont );
+            sc.close();
+        }
+    }
